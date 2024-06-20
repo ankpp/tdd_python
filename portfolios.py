634aa350eada86cad0375724ef628f163ac5f440
apnkpp <apnkpp@gmail.com>
2024-06-20 14:35:31 -0600

    Error handling for missing exchange rates

--- portfolios.py
+++ portfolios.py
@@ -20,5 +20,15 @@ class Portfolio:
         self.moneys.extend(moneys)
 
     def evaluate(self, currency):
-        total = reduce(operator.add, map(lambda m: self.__convert(m, currency), self.moneys), 0)
-        return Money(total, currency)
+        total = 0.0
+        failures = []
+        for m in self.moneys:
+            try:
+                total += self.__convert(m, currency)
+            except KeyError as ke:
+                failures.append(ke)
+        if not failures:
+            return Money(total, currency)
+        
+        failure_message = "".join(f.args[0] for f in failures)
+        raise Exception("Missing exchange rate(s):[" + failure_message + "]")
